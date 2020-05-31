3fdf5734ec2e119025113578e9cabee6b4f226bd
Vishal Sharma <adm.vshalshrma@gmail.com>
2020-05-31 18:42:07 +0530

    Cached Inverse Matrix within a new Matrix creation like function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix(x) creates a matrix with its inverse embedded/cached in itself
+## cacheSolve(x, ...) returns inverse of the matrix 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## a null matrix assigned to inverse matrix variable  
+  inv_matrix <- matrix()
+  
+  ## set the matrix with setMatrix function
+  setMatrix <- function(y) {
+    x <<- y
+    inv_matrix <<- matrix()
+  }
+  
+  ## get Matrix with getMatrix method
+  getMatrix <- function() x
+  
+  ## set the Inverse of the matrix
+  setInverseMatrix <- function() {
+    
+    inv_matrix <<- solve(x)
+    
+  }
+  
+  getInverseMatrix <- function() inv_matrix
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverseMatrix = setInverseMatrix
+       getInverseMatrix = getInverseMatrix)
+  
 }
 
 
@@ -12,4 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverseMatrix <- x$getInverseMatrix()
+        
+        if( !(is.null.DN(dimnames(inverseMatrix))) ) {
+           message("getting cached inverse matrix")
+           return(inverseMatrix)
+        }
+        
+        matrix1 <- x$getMatrix()
+        inverseMatrix <- solve(matrix1, ...)
+        x$setInversetMatrix(inverseMatrix)
+        inverseMatrix
 }
